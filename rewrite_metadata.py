fd89361695680bbf164befdd9ed125137c7cb2aa
Rainer Simon <rainer.simon@ait.ac.at>
2020-01-15 10:13:36 +0100

    Added helper script for metadata rewriting

---
+++ rewrite_metadata.py
@@ -0,0 +1,44 @@
+# A utility to re-write the annotations.jsonl file for splitting documents.
+# Takes the following input arguments
+# -d the new document ID
+# -o the old document part UUID
+# -n the new document part UUID
+import argparse
+import json
+import uuid
+import random
+import string
+
+parser = argparse.ArgumentParser(description = 'Re-writes the metadata.json file document splitting')
+parser.add_argument('-k', help="the part ID to keep")
+args = parser.parse_args()
+
+new_docid = ''.join(random.choices(string.ascii_lowercase + string.digits, k = 14))
+old_partid = args.k
+new_partid = uuid.uuid4()
+
+with open('./original/metadata.json', 'r') as infile, \
+  open(f'./original/metadata.{new_partid}.json', 'w') as outfile:
+
+  meta = json.loads(infile.read())
+
+  # Generate a new random doc ID
+  meta['id'] = new_docid
+
+  # Filter parts
+  meta['parts'] = list(filter(lambda p: p['id'] == old_partid, meta['parts']))
+  
+  outfile.write(f'{json.dumps(meta, indent=2)}')
+
+  print(f'Wrote results to ./original/metadata.{new_partid}.json')
+  print(f'Original part UUID to keep: {old_partid}')
+  print(f'New doc ID: {new_docid}')
+  print(f'New part ID: {new_partid}')
+
+  print()
+  print('Rename file with:')
+  print(f'mv ./original/parts/{old_partid}.xml ./original/parts/{new_partid}.xml')
+
+  print()
+  print('Rewrite annotations with:')
+  print(f'python rewrite_annotations -d {new_docid} -o {args.k} -n {new_partid}')
