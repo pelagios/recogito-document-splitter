4d9f38b3c5416dbc4f23295bca08eb8a143277b2
Rainer Simon <rainer.simon@ait.ac.at>
2020-01-17 13:24:52 +0100

    Started sketching out filter logic

--- intra-tei-splitting/anchor.py
+++ intra-tei-splitting/anchor.py
@@ -1,7 +1,27 @@
+# IMPORTANT: this helper ONLY works for the 
+# Periegesis annotations - it's NOT a generic 
+# anchor parser!!
+import re 
+
 class Anchor:
 
   def __init__(self, str):
-    print(str)
+    # Example:
+    # from=/TEI[1]/text[1]/body[1]/div[1]/div[1]/div[23]/div[2]/p[1]::107;to=/TEI[1]/text[1]/body[1]/div[1]/div[1]/div[23]/div[2]/p[1]::122
+    self.start, self.end = str.split(';')
+    self.from_path = self.start.split('/')
+    self.to_path = self.end.split('/')
 
-  def isTrue(self):
-    True
+    # Strip tags, keep numbers only
+    self.from_path = list(map(lambda str: int(re.sub("[^0-9]", "", str)) if '[' in str else str, self.from_path))
+    self.to_path = list(map(lambda str: int(re.sub("[^0-9]", "", str)) if '[' in str else str, self.to_path))
+
+  def isWithin(self, from_chapter, from_para, to_chapter, to_para):
+    # Chapter is path segment 6, section path segment 7
+    self.from_path[6] >= from_chapter and \
+    self.from_path[7] >= from_para and \
+    self.to_path[6] <= to_chapter and \
+    self.to_path[7] <= to_para
+
+  def isValid(self):
+    self.start.find('/') > -1 and self.end.find('/') > -1
--- intra-tei-splitting/filter_annotations.py
+++ intra-tei-splitting/filter_annotations.py
@@ -7,6 +7,13 @@ FROM_PARA = 1
 TO_CHAPTER = 2
 TO_PARA = 2
 
+def isWithin(annotation, from_chapter, from_para, to_chapter, to_para):
+  anchor = Anchor(annotation['anchor'])
+  if anchor.isValid():
+    anchor.isWithin(from_chapter, from_para, to_chapter, to_para)
+  else:
+    False
+
 with open('./original/annotations.jsonl', 'r') as infile:
 
   all_annotations = infile.readlines()
@@ -14,4 +21,6 @@ with open('./original/annotations.jsonl', 'r') as infile:
   as_json = list(map(lambda str: json.loads(str), all_annotations))
 
   # Keep only annotations on the relevant part
-  relevant_annotations = list(filter(lambda a: Anchor(a['anchor']).isTrue(), as_json))
+  relevant_annotations = list(filter(lambda a: isWithin(a, 1, 1, 10, 10), as_json))
+
+  print(f'Keeping {len(relevant_annotations)} annotations')
